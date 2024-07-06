afaa3c77ac97e1b5d3fbbbc2697c095d04c9f3ca
Anton Lashch <lashchanton@gmail.com>
2024-07-06 12:45:31 +0300

    Add drop apple and move for tree

--- Assets/Scripts/AppleTree.cs
+++ Assets/Scripts/AppleTree.cs
@@ -8,13 +8,20 @@ public class AppleTree : MonoBehaviour
     public GameObject applePrefab;
     public float speed = 1f;
     public float leftAndRightEdge = 10f;
-    public float chanceToChangeDirections = 0.1f;
+    public float chanceToChangeDirections = 0.02f;
     public float secondsBetweenAppleDrops = 1f;
 
     // Start is called before the first frame update
     void Start()
     {
+        Invoke("DropApple", 2f);
+    }
 
+    void DropApple()
+    {
+        GameObject apple = Instantiate<GameObject>(applePrefab);
+        apple.transform.position = transform.position;
+        Invoke("DropApple",secondsBetweenAppleDrops);
     }
 
     // Update is called once per frame
@@ -23,5 +30,22 @@ public class AppleTree : MonoBehaviour
         Vector3 pos = transform.position;
         pos.x += speed * Time.deltaTime;
         transform.position = pos;
+
+        if (pos.x < -leftAndRightEdge)
+        {
+            speed = Mathf.Abs(speed);
+        }
+        else if (pos.x > leftAndRightEdge)
+        {
+            speed = -Mathf.Abs(speed);
+        }
+        
+    }
+    private void FixedUpdate()
+    {
+        if (Random.value < chanceToChangeDirections)
+        {
+            speed *= -1;
+        }
     }
 }
